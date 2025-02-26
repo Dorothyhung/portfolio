39d303ec1b03316d5ec787b773b8bc039ed10f10
Dorothy Hung <dorothyh0602@gmail.com>
2025-02-25 17:32:16 -0800

    Add navbar for small screens

--- components/logo.js
+++ components/logo.js
@@ -5,15 +5,15 @@ import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
     font-weight: bold;
-    font-size: 10px;
+    font-size: 20px;
     display: inline-flex;
     align-items: center;
     height: 30px;
-    line-height: 20px;
+    line-height: 40px;
     padding: 10px;
 
     &:hover img {
-        transform: rotate(20deg);
+        transform: rotate(330deg);
     }
 `
 
@@ -23,7 +23,7 @@ const Logo = () => {
     return (
         <Link href="/">
             <LogoBox>
-                <Image src={footPrintImg} width={20} height={20} alt="logo"/>
+                <Image src={footPrintImg} width={30} height={30} alt="logo"/>
                 <Text
                     color={useColorModeValue('gray.800', 'whiteAlpha.900')}
                     fontFamily='M PLUS Rounded 1c'
--- components/navbar.js
+++ components/navbar.js
@@ -35,7 +35,7 @@ const Navbar = props => {
     const { path } = propNames
     return (
         <Box
-            position="fixed"
+            // position="fixed"
             as="nav"
             v="100%"
             bg={useColorModeValue('#ffffff40', '#20202300')}
@@ -66,12 +66,28 @@ const Navbar = props => {
                     >
                         <LinkItem href="/projects" path={path}>Projects</LinkItem>
                         <LinkItem href="/works" path={path}>Works</LinkItem>
+                        <LinkItem href="https://github.com/Dorothyhung/portfolio" target="_blank" path={path}>Source</LinkItem>
+                        <LinkItem href="https://www.linkedin.com/in/dorothyhung1/" target="_blank" path={path}>Source</LinkItem>
+                        
                     </Stack>
 
-                    <Box flex={1} align="right">
+                    <Box flex={1} align="center">
                         <Box ml={2} display={{base: 'inline-block', md: 'none'}}>
                             <Menu>
                                 <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline" aria-label="0ptions" />
+                                <MenuList>
+                                    <Link href="/">
+                                        <MenuItem>About</MenuItem>
+                                    </Link>
+                                    <Link href="/projects">
+                                        <MenuItem>Projects</MenuItem>
+                                    </Link>
+                                    {/* <NextLink href="/projects" passHref> */}
+                                        {/* <MenuItem as={Link}>Projects</MenuItem> */}
+                                    {/* </NextLink> */}
+                                    <MenuItem as={Link} href="https://github.com/Dorothyhung/portfolio" target="_blank">View Source</MenuItem>
+                                    <MenuItem as={Link} href="https://www.linkedin.com/in/dorothyhung1/" target="_blank">LinkedIn</MenuItem>
+                                </MenuList>
                             </Menu>
                         </Box>
                     </Box>
